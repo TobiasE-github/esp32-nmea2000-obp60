97c0d15ca4ac283168cae87a296765137e65bef3
Tobias Edler <tobias.edler@gmail.com>
2026-03-14 18:54:17 +0100

    select opb60 via compile script

--- compile.sh
+++ compile.sh
@@ -10,6 +10,6 @@
 #clean all
 #~/.platformio/penv/bin/pio run -t fullclean
 
-~/.platformio/penv/bin/pio run
+~/.platformio/penv/bin/pio run -e obp60_s3
 
 for i in .pio/build/obp60_s3/obp60_s3-*-update.bin ;do cp -v $i ~/OBP-Images/`basename $i .bin``date +%H%M`.bin; done
