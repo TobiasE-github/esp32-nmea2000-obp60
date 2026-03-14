d1236c8ce09d1bf55845720b4b984575466be4f6
TobiasE-github <tobias.edler@gmail.com>
2026-03-14 19:01:48 +0100

    Cleanup compile script

--- compile.sh
+++ compile.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#on gitpod
-#cd /workspace/esp32-nmea2000-obp60 
-#bash lib/obp60task/run_install_tools 
-#bash lib/obp60task/run_obp60_s3 
 
-#locally:
 #Update Libs 
 #~/.platformio/penv/bin/pio pkg update
 #clean all
